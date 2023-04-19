0bca6b063c1103cba0116c2efad4df96f9104647
SteDiamant <123244340+SteDiamant@users.noreply.github.com>
2023-04-19 11:37:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ st. set_page_config(layout="wide")
 
 class DataLoader():
     def load_data():
-        path=os.path.join('data','data_original.csv')
+        os.chdir(os.path.dirname('data/data_original.csv'))
+        
         #print(path)
         ##THERE IS A BUG HERE WHEN I RUN THE CODE ON STREAMLIT CLOUD I HAVE TO DELETE THE {data}/data_original.csv FILE AND RUN THE CODE AGAIN
-        df = pd.read_csv(path)
+        df = pd.read_csv(r'data/data_original.csv')
         return df
 
 class PlotOptions():
