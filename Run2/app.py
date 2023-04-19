1ad81124e530c2389cf5e7637e028ce3db511bfa
SteDiamant <123244340+SteDiamant@users.noreply.github.com>
2023-04-19 15:45:51 +0200

    Update app.py

--- Run2/app.py
+++ Run2/app.py
@@ -133,7 +133,7 @@ def load_data():
         #df1 = pd.read_csv(r"strategies/strategy_0.csv")
         #df2 = pd.read_csv(r"strategies/strategy_1.csv")
         ###FOR GITHUB
-        df1 = pd.read_csv(r"Run2/strategies/strategy_1.csv")
+        df1 = pd.read_csv(r"Run2/strategies/strategy_0.csv")
         df2 = pd.read_csv(r"Run2/strategies/strategy_1.csv")
         df2.rename(columns={'Unnamed: 0':'Time'},inplace=True)
         df1['PV (W)']=df1['PV (W)']*327
